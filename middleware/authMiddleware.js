f1ee55fd61d98377eda4d88f63df012f79b9a14e
Jannat <jannatfbf@gmail.com>
2023-11-15 18:11:36 +0530

    first commit

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -13,14 +13,13 @@ const protect = asyncHandler(async (req, res, next) => {
       // Get token from header
       token = req.headers.authorization.split(' ')[1]
       // Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      const decoded = jwt.verify(token, process.env.JWT_SECRET | abc123)
       // Get user from token
       req.user = await User.findById(decoded.id).select('-password')
-      // NOTE: We need to check if a user was found
-      // https://www.udemy.com/course/react-front-to-back-2022/learn/lecture/30591026#questions/17843570
+      
       if (!req.user) {
         res.status(401)
-        throw new Error('Not authirised')
+        throw new Error('Not authorised')
       }
 
       next()
