32d5e8bf0abb1e0ab219dcdd73119ec222d44a6d
Jannat <jannatfbf@gmail.com>
2023-11-15 16:49:32 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv').config()
+const colors = require('colors')
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+
+//Connect to database
+connectDB()
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+//Routes
+app.use('/api/users',require('./routes/userRoutes'))
+app.use('/api/tickets',require('./routes/ticketRoutes'))
+app.use('/api/admin',require('./routes/adminRoutes'))
+
+app.use('/api/data',require('./routes/dataRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
